1c52e6123f404911d7cbc3403f61bca369160c93
Lefchakk <joao.lefchak@edu.unipar.br>
2024-08-19 22:41:08 -0300

    Inicial

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main (String[] args) {
+        //Função padrão//
+        //Mostra ou carrega imagem na tela//
+        System.out.println("Hi Java, nice to meet you!");
+
+        ////////////////Váriaveis///////////////////
+
+        //String é uma escrita//
+            String nome = "Jhon";
+            char sexo = 'M';
+
+        // [] -> Diz que é um array//
+        // {} -> Indicar um objeto//
+            char[] textoChar = {'5'};
+
+        //Guarda valores Reais//
+            double altura = 1.76;
+            Double peso = 68.0;
+            float alturaPessoa2 = 1.80f;
+            Float alturaPessoa3 = 1.83f;
+
+        //Guardar valores inteiros//
+            int numeroCasa = 20;
+            Integer numeroAlunos = 75;
+            Long numeroGolsDoBraitwaith = 3l;
+
+        /////////////////Operadores aritmeticos//////////////////////
+
+            Integer soma = 5+5;
+            Integer subtracao = 52-2;
+            Integer multiplicar = 10*5;
+            Integer divisao = 100/5;
+
+            Integer resultadoSoma = soma + subtracao;
+            Integer resultadoSubtracao = subtracao - multiplicar;
+            Integer resultadoMultiplicacao = multiplicar * 2;
+            Integer resultadoDivisao = divisao / 2;
+
+
+            System.out.println("Valor da multiplicação é : " + resultadoMultiplicacao);
+            System.out.println("Valor da Subtração é : " + resultadoSubtracao);
+            System.out.println("Valor da Divisão é : " + resultadoDivisao);
+
+        }
+    }
